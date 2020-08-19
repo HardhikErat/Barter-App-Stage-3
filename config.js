e94361f6245917e39dbf6f90e056e51202a9a3f9
HardhikErat <58391944+HardhikErat@users.noreply.github.com>
2020-08-19 15:44:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 require('@firebase/firestore')
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBVdqWajEpMrgN8DcdqhbdUK6AkbGP-g8E",
-    authDomain: "bartar-system.firebaseapp.com",
-    databaseURL: "https://bartar-system.firebaseio.com",
-    projectId: "bartar-system",
-    storageBucket: "bartar-system.appspot.com",
-    messagingSenderId: "573044276501",
-    appId: "1:573044276501:web:f6f2ef6db00ef2683efd1c",
-    measurementId: "G-YWPCLZGHR3"
+    apiKey: "AIzaSyBj4XOWtHo_2_MbDWL0ZD1XGcbDmalbECI",
+    authDomain: "barter-app-df844.firebaseapp.com",
+    databaseURL: "https://barter-app-df844.firebaseio.com",
+    projectId: "barter-app-df844",
+    storageBucket: "barter-app-df844.appspot.com",
+    messagingSenderId: "973197261150",
+    appId: "1:973197261150:web:ad0bcb7aa88c91bf6c37cf",
+    measurementId: "G-JYSER9X076"
   };
   // Initialize Firebase
 
